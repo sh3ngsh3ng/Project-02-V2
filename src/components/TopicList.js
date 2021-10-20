2c395c79e14d40392a82e7385b9958ce7431f4b4
sh3ngsh3ng <yshenglim13@gmail.com>
2021-10-20 04:50:24 +0000

    linked topic list with subject selection, added default data into data.json

--- src/components/TopicList.js
+++ src/components/TopicList.js
@@ -3,13 +3,11 @@ import React from "react"
 export default function TopicList (props) {
     let levelObj = props.levelObj(props.data, props.selectedLevel)
     let selectedSubject = props.selectedSubject
-    let subArray = levelObj[selectedSubject]
-
     return (
         <select>
-            {/* {subArray.map(function(topic) {
+            {levelObj[selectedSubject].map(function(topic) {
                 return <option>{topic}</option>
-            })} */}
+            })}
         </select>
 
     )
