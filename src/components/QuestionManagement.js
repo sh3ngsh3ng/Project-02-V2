9de3e4709e0bf3a7f4ae68c23be13d09ccb95304
sh3ngsh3ng <yshenglim13@gmail.com>
2021-10-26 02:31:37 +0000

    added active class based on chosen tab

--- src/components/QuestionManagement.js
+++ src/components/QuestionManagement.js
@@ -2,9 +2,33 @@ import React from "react"
 
 
 export default class QuestionManagement extends React.Component {
+    state = {
+        'active': "submissions",
+
+    }
 
 
 
+
+
+    renderContent() {
+        if (this.state.active == "addnew") {
+            return("addnew")
+        } else if (this.state.active == "submissions") {
+            return ("submissions")
+        }
+    }
+
+
+    setActive = (page) => {
+        this.setState({
+            'active': page
+        })
+    }
+
+    setActiveTab = (evt) => {
+        evt.target.className = "nav-link active"
+    }
 
 
     render() {
@@ -12,12 +36,21 @@ export default class QuestionManagement extends React.Component {
             <React.Fragment>
                 <ul class="nav nav-tabs">
                     <li class="nav-item">
-                        <button class="nav-link">Submissions</button>
+                        <button className={`nav-link ${this.state.active === "submissions" ? "active" : null}`} 
+                                onClick={(evt) => {
+                                    this.setActive('submissions')
+                                }}
+                        >Submissions</button>
                     </li>
                     <li class="nav-item">
-                        <button class="nav-link active" >Add New</button>
+                        <button className= {`nav-link ${this.state.active === "addnew" ? "active" : null}`} 
+                                onClick ={(evt) => {
+                                    this.setActive('addnew')
+                                }}
+                        >Add New</button>
                     </li>
                 </ul>
+                {this.renderContent}
             </React.Fragment>
         )
     }
