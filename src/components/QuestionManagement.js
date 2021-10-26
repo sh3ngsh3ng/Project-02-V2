eedb57e98312b116f33085146a3f762341143faf
sh3ngsh3ng <yshenglim13@gmail.com>
2021-10-26 04:02:34 +0000

    Added QuestionDisplay Compoenent for user to view their submitted questions

--- src/components/QuestionManagement.js
+++ src/components/QuestionManagement.js
@@ -1,24 +1,38 @@
+import axios from "axios"
 import React from "react"
 import QuestionForm from "./QuestionForm"
+import QuestionDisplay from "./QuestionDisplay"
 
 
 export default class QuestionManagement extends React.Component {
     state = {
         'active': "submissions",
+        "contributions": []
 
     }
 
+    url = "https://3000-olive-rooster-dsty3hak.ws-us17.gitpod.io/"
 
 
+    async componentDidMount() {
+        let contributions = (await axios.get(this.url)).data
+        this.setState({
+            contributions
+        })
+    }
 
 
     renderContent() {
         if (this.state.active == "addnew") {
             return(
+                // Form to addnew
                 <QuestionForm levelObj = {this.props.levelObj}/>
             )
         } else if (this.state.active == "submissions") {
-            return ("submissions")
+            // List of Contributed questions
+            return (
+                <QuestionDisplay searchResults={this.state.contributions}/>
+            )
         }
     }
 
