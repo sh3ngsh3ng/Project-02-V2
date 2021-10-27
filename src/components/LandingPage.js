ce24444fbb77b52c7f76d54d181e281f6e2acec7
sh3ngsh3ng <yshenglim13@gmail.com>
2021-10-27 07:37:15 +0000

    added motion for find question button

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,12 +1,21 @@
 import React from "react"
-import LevelBtn from "./LevelBtn"
+import {motion} from "framer-motion"
 
 
 export default function LandingPage (props) {
     return (
         <React.Fragment>
             <div>
-                <button id="start-btn" onClick={props.changePage}>Find Questions</button>
+                <motion.button id="start-btn" 
+                        onClick={props.changePage}
+                        animate={{x:0}}
+                        initial={{x:"-100%"}}
+                        transition={{
+                            type:"spring",
+                            stiffness:50,
+                        }}
+                >Find Questions
+                </motion.button>
             </div>
         </React.Fragment>
 
