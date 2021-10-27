06acd515664116533d9007625f02737337c79ec8
sh3ngsh3ng <yshenglim13@gmail.com>
2021-10-27 08:07:38 +0000

    added animation to grade buttons using variants

--- src/components/GradeRadioBtn.js
+++ src/components/GradeRadioBtn.js
@@ -1,8 +1,23 @@
 import React from "react"
-
+import {motion} from "framer-motion"
 
 export default function GradeRadioBtn (props) {
     let levelObj = props.levelObj(props.data, props.selectedLevel)
+
+    const variants = {
+        animate: {
+            x:0,
+            opacity: 1
+        },
+        initial: {
+            x: "200%",
+            opacity: 0.5
+        },
+        transition: {
+            type: "spring",
+            stiffness: 50,
+        }
+    }
 
     return (
         <React.Fragment>
@@ -10,13 +25,21 @@ export default function GradeRadioBtn (props) {
             {levelObj.grade.map(function(grade){
                 return (
                     <React.Fragment>
-                        <input type="radio" 
+                        <motion.input type="radio" 
                                 name="selectedGrade" 
                                 value={grade} 
                                 checked={props.selectedGrade == grade}
                                 onChange = {props.updateFormField}
+                                variants = {variants}
+                                animate = "animate"
+                                initial = "initial"
+                                transition = "transition"
                                 />
-                        <span>{grade}</span>
+                        <motion.span variants={variants}
+                                        animate ="animate"
+                                        initial = "initial"
+                                        transiition = "transition"
+                        >{grade}</motion.span>
                     </React.Fragment>
 
                 )
--- src/components/LevelBtn.js
+++ src/components/LevelBtn.js
@@ -1,16 +1,30 @@
 import React from "react"
+import {motion} from "framer-motion"
 
 export default function LevelBtn (props) {
     return (
         <React.Fragment>
-            {props.data.map(function(obj) {
-                return <button type="button"
+            {props.data.map(function(obj, i) {
+                return <motion.button type="button"
                             key = {obj.id} 
                             value={obj.value}
                             className="btn btn-primary"
                             name = "selectedLevel"
                             onClick = {props.updateFormField}
-                            >{obj.level}</button>
+                            animate={{
+                                x:0,
+                                opacity: 1
+                            }}
+                            initial={{
+                                x:"200%",
+                                opacity: 0.5
+                            }}
+                            transition={{
+                                type:"spring",
+                                stiffness: 50,
+                                delay: i * 0.03
+                            }}
+                            >{obj.level}</motion.button>
             })}
             
         </React.Fragment>
