39d1ee5c0817e07a51dd7636748ddac081f7ad86
sh3ngsh3ng <yshenglim13@gmail.com>
2021-10-27 07:39:40 +0000

    added animation for navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,23 @@
 import React from "react"
+import {motion} from "framer-motion"
 
 export default function NavBar(props) {
     return (
         <React.Fragment>
-                <nav className="navbar navbar-light bg-light">
+            <motion.nav className="navbar navbar-light bg-light"
+                        animate={{y:0}}
+                        initial={{y:"-100%"}}
+                        transition={{
+                            type:"spring",
+                            stiffness: 50,
+                            delay: 0.02
+                        }}
+            >
                 <div className="container-fluid">
                     <span className="navbar-brand mb-0 h1">Navbar</span>
                     <button className="btn btn-primary" onClick={props.login}>Login</button>
                 </div>
-            </nav>
+            </motion.nav>
         </React.Fragment>
     )
 }
